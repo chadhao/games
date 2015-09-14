fc001e2e031e217eb92a277189d37117514ba4d4
Hao Duan <haoduan@outlook.com>
2015-09-14 15:42:05 +1200

    class Player

--- PopStar/src/Player.java
+++ PopStar/src/Player.java
@@ -1,5 +1,35 @@
 
 public class Player
 {
-
+	private String name;
+	private int score;
+	
+	public Player(String name)
+	{
+		this.name = name;
+		this.score = 0;
+	}
+	
+	public Player()
+	{
+		this.name = "UNKNOWN";
+		this.score = 0;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public void setScore(int score)
+	{
+		this.score = score;
+	}
+	
+	
 }
